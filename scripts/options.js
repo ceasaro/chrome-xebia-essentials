8dc54a0b5aa9f7b17f75d24787c791c907d21e1a
ceasaro <ceesvw@gmail.com>
2023-02-11 17:01:55 +0100

    option to post selected xebia card in slack

--- scripts/options.js
+++ scripts/options.js
@@ -1,15 +1,19 @@
 // Saves options to chrome.storage
 function save_options() {
-  let slackToken = document.getElementById('slack_token').value;
-  chrome.storage.sync.set({
-    slackToken: slackToken,
-  }, function() {
-    // Update status to let user know options were saved.
-    let status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
+  let slackToken = document.getElementById("slack_token").value;
+  let slackChannel = document.getElementById("slack_channel").value;
+  chrome.storage.sync.get({ "xebiaStorageKey": DEFAULT_XEBIA_STORAGE }).then((result) => {
+    let xebiaState = result.xebiaStorageKey;
+    xebiaState.slack.token = slackToken;
+    xebiaState.slack.channel = slackChannel;
+    chrome.storage.sync.set({ xebiaStorageKey: xebiaState }).then(() => {
+      // Update status to let user know options were saved.
+      let status = document.getElementById("status");
+      status.textContent = "Options saved.";
+      setTimeout(function () {
+        status.textContent = "";
+      }, 750);
+    });
   });
 }
 
@@ -17,12 +21,13 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    slackToken: '',
-  }, function(items) {
-    document.getElementById('slack_token').value = items.slackToken;
+  chrome.storage.sync.get({ "xebiaStorageKey": DEFAULT_XEBIA_STORAGE }, function (result) {
+    let xebiaState = result.xebiaStorageKey;
+    document.getElementById("slack_token").value = xebiaState.slack.token;
+    document.getElementById("slack_channel").value = xebiaState.slack.channel;
   });
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);Id('save').addEventListener('click', save_options);
+
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click", save_options);
+Id("save").addEventListener("click", save_options);
